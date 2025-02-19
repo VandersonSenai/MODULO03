df7b03161e84c0937a3fca3174a15a468aeb88cc
VandersonSenai <vanderson.silva13@aluno.senai.br>
2025-02-19 19:55:42 -0300

    DOM e a Árvore

---
+++ Introducao ao JavaScritp/js/meu_arquivo_javascript.js
@@ -0,0 +1,14 @@
+var p = document.getElementById('id_p');
+console.log(p);
+
+window.onload = function() {
+    
+    // Localiza o elemento com id "id_p" e armazena na variável p
+    var p = document.getElementById('id_p');
+
+    // Altera o conteúdo do elemento p
+    p.style.backgroundColor = 'red';
+    
+    console.log(p);
+
+}
