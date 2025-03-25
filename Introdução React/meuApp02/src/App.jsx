04345939a506df5a48a124f5bcee58c78249ef01
VandersonSenai <vanderson.silva13@aluno.senai.br>
2025-03-24 21:27:16 -0300

    if ternário alterando o CSS

--- Introdução React/meuApp02/src/App.jsx
+++ Introdução React/meuApp02/src/App.jsx
@@ -1,40 +1,39 @@
-import { useState } from 'react'
-import './App.css'
-import ShowUserName from './components/ShowUserName'
-import Time from './components/Time'
-import ListaCantor from './components/ListaCantor'
+import { useState } from "react";
+import "./App.css";
+import ShowUserName from "./components/ShowUserName";
+import Time from "./components/Time";
+import ListaCantor from "./components/ListaCantor";
 
 function App() {
-  const nome = "Vanderson"
-  const [name] = useState("Ney")
+  const nj = 6;
+  const nome = "Vanderson";
+  const [name] = useState("Ney");
   const [cantor] = useState([
     {
-      id:1, 
-      nome:"Bruce Dickson", 
-      estiloMusical:"RnR", 
-      premiado:true,
-    },    
+      id: 1,
+      nome: "Bruce Dickson",
+      estiloMusical: "RnR",
+      premiado: true,
+    },
     {
-      id:1, 
-      nome:"Fredy Mercury", 
-      estiloMusical:"RnR", 
-      premiado:true,
-    },    
+      id: 1,
+      nome: "Fredy Mercury",
+      estiloMusical: "RnR",
+      premiado: true,
+    },
     {
-      id:1, 
-      nome:"Tiririca", 
-      estiloMusical:"PoP", 
-      premiado:false,
-    }
-]);
+      id: 1,
+      nome: "Tiririca",
+      estiloMusical: "PoP",
+      premiado: false,
+    },
+  ]);
   return (
     <>
-    {/* passando para o componente ShowUserName
+      {/* passando para o componente ShowUserName
     os parametros name e shirt 
     */}
-      <ShowUserName name={nome}
-      shirt="7" />
-
+      <ShowUserName name={nome} shirt="7" />
 
       {/* Props com desestruturação */}
       {/* <hr />
@@ -43,26 +42,42 @@ function App() {
       <Time nome="Real Madrid" estado="MD" posicao={2} campeao={true}/>
       <hr/>
       <Time nome="Vasco da Gama" estado="RJ" posicao={100} campeao={false}/> */}
-      <hr/>
-      <hr/>
+      <hr />
+      <hr />
       {
         // Posicao dentro da matriz
         cantor.map((item) => (
           <ListaCantor
-          
-          key={item.id} nome={item.nome} estiloMusical={item.estiloMusical} premiado={item.premiado}
+            key={item.id}
+            nome={item.nome}
+            estiloMusical={item.estiloMusical}
+            premiado={item.premiado}
           />
         ))
       }
-{/*       
+
+      <h2
+      // usamo esse IF para verificar se nj>5
+        style={
+          nj > 5
+            ? { color: "green", backgroundColor: "yellow", fontSize: "30px" }
+            : { color: "red", backgroundColor: "white", fontSize: "20px" }
+        }
+      >
+        Este CSS é dinâmico
+      </h2>
+      <h2       // usamo esse IF para verificar se nj>5
+        style={
+          nj < 5
+            ? { color: "green", backgroundColor: "yellow", fontSize: "30px" }
+            : { color: "red", backgroundColor: "white", fontSize: "20px" }
+        }>Este CSS também é dinâmico</h2>
+      {/*       
       <ListaCantor
       key="" nome={cantor.nome} estiloMusical={cantor.estiloMusical} premiado={cantor.premiado}
       /> */}
-
-
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
