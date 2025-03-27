48688e6d2b2f85931c51dd93e1d28848ddcfc104
kallangos <vandersondasilva@gmail.com>
2025-03-27 18:00:23 -0300

    Link ativos em destaque

--- Introdução React/meuApp02/src/components/NavBar.jsx
+++ Introdução React/meuApp02/src/components/NavBar.jsx
@@ -1,18 +1,64 @@
-import { NavLink } from "react-router-dom"  // componente para manipular barra de navegação
+import { NavLink } from 'react-router-dom' // componente para manipular barra de navegação
 import React from 'react'
-import styles  from "./css/NavBar.module.css" //importar o CSS especifico para esta navbar "impede que o CSS vaze para outros componentes"
+import styles from './css/NavBar.module.css' //importar o CSS especifico para esta navbar "impede que o CSS vaze para outros componente
 
 const NavBar = () => {
   return (
-
-        <nav className={styles.navbar}>
-            <NavLink to="/" className={styles.link}>Home</NavLink>
-            <NavLink to="/Login" className={styles.link}>Login</NavLink>
-            <NavLink to="/Cadastro" className={styles.link}>Cadastro</NavLink>
-            <NavLink to="/Sobre" className={styles.link}>Sobre</NavLink>
-            <NavLink to="/Contato" className={styles.link}>Contato</NavLink>
-        </nav>   
-
+    <nav className={styles.navbar}>
+      <NavLink
+        to="/"
+        className={styles.link}
+        style={({ isActive }) => {
+          return { color: isActive ? '#5e63ff' : '#e6e6ef' }
+        }}
+        className={styles.link}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/Login"
+        className={styles.link}
+        style={({ isActive }) => {
+          return { color: isActive ? '#5e63ff' : '#e6e6ef' }
+        }}
+        className={styles.link}
+      >
+        Login
+      </NavLink>
+      <NavLink
+        to="/Cadastro"
+        className={styles.link}
+        style={({ isActive }) => {
+          return { color: isActive ? '#5e63ff' : '#e6e6ef' }
+        }}
+        className={styles.link}
+      >
+        Cadastro
+      </NavLink>
+      <NavLink
+        to="/Sobre"
+        className={styles.link}
+        style={({ isActive }) => {
+          return { color: isActive ? '#5e63ff' : '#e6e6ef' }
+        }}
+        className={styles.link}
+      >
+        Sobre
+      </NavLink>
+      <NavLink
+        to="/Contato"
+        style={({ isActive }) => {
+          return { color: isActive ? '#5e63ff' : '#e6e6ef' }
+        }}
+        className={styles.link}
+        style={({ isActive }) => {
+          return { color: isActive ? '#5e63ff' : '#e6e6ef' }
+        }}
+        className={styles.link}
+      >
+        Contato
+      </NavLink>
+    </nav>
   )
 }
 
