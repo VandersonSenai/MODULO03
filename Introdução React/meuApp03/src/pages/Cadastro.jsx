7210ff269d045fb8c26a22b3ffb95578fe2094a6
VandersonSenai <vanderson.silva13@aluno.senai.br>
2025-03-28 21:43:36 -0300

    Criando tela de login

--- Introdução React/meuApp03/src/pages/Cadastro.jsx
+++ Introdução React/meuApp03/src/pages/Cadastro.jsx
@@ -7,25 +7,25 @@ const Cadastro = () => {
     
     <div className={Styles.div}>
 
-      <div >
-        <label for="nome" className={Styles.label}>Nome : </label>
+      <div className={Styles.filed}>
+        <label for="nome" >Nome : </label>
         <input type="text" name="nome" id="nome" required placeholder="Vanderson da Silva"></input>
       </div>
 
       <div>
-        <label for="mail">e-mail : </label>
+        <label for="email"className={Styles.label}>e-mail : </label>
         <input type="mail" name="mail" id="email" required placeholder="teste@mail.com"></input>
       </div>
       <div>
-        <label for="mail">Senha : </label>
-        <input type="password" id="pass" name="password" minlength="8" required />
+        <label for="password">Senha : </label>
+        <input type="password" id="password" name="password" minlength="8" required />
       </div>
 
       <div>
-        <label for="mail">Confirmação de Senha : </label>
-        <input type="password" id="pass" name="password" minlength="8" required />
+        <label for="passwordConf">Confirmação de Senha : </label>
+        <input type="password" id="passwordConf" name="passwordConf" minlength="8" required />
       </div>
-
+      
 
     </div>
 
