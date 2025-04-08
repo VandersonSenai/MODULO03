402d93758b7b6c84ea614927ce2aea9c772e5341
VandersonSenai <vanderson.silva13@aluno.senai.br>
2025-04-08 19:10:02 -0300

    meuApp03_Greg

---
+++ Introdução React/meuApp03_Greg/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import NavBar from "./components/NavBar";
+
+//Importação do React-Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return(
+    <>
+      <NavBar />
+      <Outlet />
+    
+    </>
+  )
+}
+
+export default App;
---
+++ Introdução React/meuApp03_Greg/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import Alert from "react-bootstrap/Alert";
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      <Alert variant="danger">
+        Hackearam meu email, Hackearam meu site
+      </Alert>
+    </div>
+  );
+};
+
+export default Login;
